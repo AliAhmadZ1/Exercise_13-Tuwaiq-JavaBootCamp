2af172f6687aee3bb3b7abf9d0c00226be690927
aliali <aliali@test.com>
2025-03-10 13:08:38 +0300

    start

---
+++ src/main/java/com/example/exercise_13tuwaiqjavabootcamp/Exercise13TuwaiqJavaBootCampApplication.java
@@ -0,0 +1,13 @@
+package com.example.exercise_13tuwaiqjavabootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise13TuwaiqJavaBootCampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise13TuwaiqJavaBootCampApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/exercise_13tuwaiqjavabootcamp/Exercise13TuwaiqJavaBootCampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercise_13tuwaiqjavabootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise13TuwaiqJavaBootCampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
